6a6ab854fefa78ec205f1a256d08287c5660b251
Alejandro <alejandro.rodriguezdiego@tajamar365.com>
2025-02-21 09:45:56 +0100

    Realiza las migraciones para aplicar los modelos en la DB, modifica el archivo admin.py

--- LaCuchara/la_cuchara_app/admin.py
+++ LaCuchara/la_cuchara_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Restaurante, Menu, Plato, ValoracionRestaurante, ValoracionPlato, Promocion, Reserva
+
+admin.site.register(Restaurante)
+admin.site.register(Menu)
+admin.site.register(Plato)
+admin.site.register(ValoracionRestaurante)
+admin.site.register(ValoracionPlato)
+admin.site.register(Promocion)
+admin.site.register(Reserva)
