1bd3312b916a130014c53e4bd1ea9b1c4ee0d318
Alejandro <alejandro.rodriguezdiego@tajamar365.com>
2025-02-26 09:44:37 +0100

    Formatea los resultados de la búsqueda de restaurante

--- LaCuchara/la_cuchara_app/views.py
+++ LaCuchara/la_cuchara_app/views.py
@@ -1,11 +1,38 @@
+import re
 from django.shortcuts import render
-from django.http import HttpResponse
 from .forms import BusquedaForm
 from .blob_storage import buscar_en_blobs
 
 # Vista principal con enlace a búsqueda
 def home(request):
     return render(request, 'la_cuchara_app/home.html')
+
+def formatear_nombre_archivo(nombre_archivo):
+    """
+    Convierte un nombre de archivo en un formato más legible.
+    Ejemplo: 'menu-Margarito_Ponzano-Bar.pdf.txt' -> 'Menú: Bar Margarito Ponzano'
+    """
+    # Quitar la extensión
+    nombre_archivo = nombre_archivo.replace(".pdf.txt", "")
+
+    # Separar partes por el guion
+    partes = nombre_archivo.split('-')
+
+    if len(partes) < 2:
+        return nombre_archivo  # Devolver sin cambios si el formato no es válido
+    
+    tipo_menu = partes[0]  # 'menu', 'carta', etc.
+    nombre_lugar = partes[1:-1]  # El nombre del sitio
+    tipo_lugar = partes[-1]  # 'Restaurante', 'Bar', etc.
+
+    # Reemplazar guiones bajos por espacios en el nombre del lugar
+    nombre_lugar = " ".join(nombre_lugar).replace("_", " ")
+
+    # Convertir el tipo de menú a un formato más bonito
+    tipo_menu = tipo_menu.capitalize().replace("_", " ")  # 'menu_del_dia' -> 'Menu del dia'
+
+    # Formatear el resultado
+    return f"{tipo_menu}: {tipo_lugar} {nombre_lugar}"
 
 def buscar(request):
     resultados = []
@@ -15,8 +42,10 @@ def buscar(request):
         
         if form.is_valid():
             palabra_clave = form.cleaned_data['palabra_clave']
-            # Llamar a la función de búsqueda en los blobs
-            resultados = buscar_en_blobs(palabra_clave)
+            archivos = buscar_en_blobs(palabra_clave)
+
+            # Formatear los nombres de los archivos
+            resultados = [formatear_nombre_archivo(archivo) for archivo in archivos]
     
     else:
         form = BusquedaForm()
